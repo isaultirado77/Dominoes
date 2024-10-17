096a0c6b4d3abe1aa5c0981e4f34ba4fe4fd8f64
Isaul <tiradolisaul@gmail.com>
2024-10-16 20:57:14 -0700

    stage 1 implemented: testing

--- dominoes/dominoes.py
+++ dominoes/dominoes.py
@@ -34,8 +34,8 @@ class Player(ABC):
     def move(self):
         pass
 
-    def take_pieces(self, pieces: list) -> None:
-        self.pieces = pieces
+    def take_piece(self, piece: list) -> None:
+        self.pieces.append(piece)
 
     def get_snake(self):
         snakes = [x for x in self.pieces if x[0] == x[1]]
@@ -51,6 +51,12 @@ class Player(ABC):
                     max_sum = current_sum
             return [max_sum / 2, max_sum / 2]
 
+    def get_pieces(self):
+        return self.pieces
+
+    def drop_piece(self, piece: list) -> None:
+        self.pieces.remove(piece)
+
 
 class Human(Player):
     def __init__(self, name: str = None):
@@ -59,6 +65,9 @@ class Human(Player):
     def move(self):
         print(f'{self.name} -> moving...')
         pass
+
+    def __str__(self):
+        return 'Player'
 
 
 class Computer(Player):
@@ -69,6 +78,9 @@ class Computer(Player):
         print(f'{self.name} -> calculating...')
         pass
 
+    def __str__(self):
+        return 'Computer'
+
 
 class Engine:
     def __init__(self):
@@ -76,19 +88,38 @@ class Engine:
         self.human = Human()
         self.bot = Computer()
         self.isHumanTurn = True
+        self.snake = None
 
-    def deal_dominoes(self, player: Player) -> None:
+    def deal_dominoes(self) -> None:
         for i in range(7):
-            piece = self.box.get_piece()
-
-    def change_turn(self):
-        return True if self.isHumanTurn else False
-
-    def display_status(self):
-        pass
+            human_piece = self.box.get_piece()
+            bot_piece = self.box.get_piece()
+            self.human.take_piece(human_piece)
+            self.bot.take_piece(bot_piece)
 
     def get_first_player(self):
-        pass
+        human_snake = self.human.get_snake()
+        bot_snake = self.bot.get_snake()
+        if not human_snake and not bot_snake:
+            self.deal_dominoes()
+            self.get_first_player()
+        if sum(bot_snake) > sum(bot_snake):
+            self.snake = bot_snake
+            self.bot.drop_piece(bot_snake)
+            self.switch_turn()
+        else:
+            self.snake = human_snake
+            self.human.drop_piece(human_snake)
+
+    def switch_turn(self):
+        return True if self.isHumanTurn else False
+
+    def display_game_state(self):
+        print('Stock pieces:', self.box.arr)
+        print('Computer pieces:', self.bot.get_pieces())
+        print('Player pieces:', self.human.get_pieces())
+        print('Domino snake:', self.snake)
+        print('Status:', self.human if self.isHumanTurn else self.bot)
 
     @staticmethod
     def get_engine():
@@ -96,11 +127,18 @@ class Engine:
 
 
 class Controller:
-    pass
+    def __init__(self):
+        self.engine = Engine.get_engine()
+
+    def run(self):
+        self.engine.deal_dominoes()
+        self.engine.get_first_player()
+        self.engine.display_game_state()
 
 
 def main():
-    pass
+    controller = Controller()
+    controller.run()
 
 
 if __name__ == "__main__":
